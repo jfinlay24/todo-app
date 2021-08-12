3730ad0367307ae92ffaeab4b7217e56a6da34eb
Shane O Dwyer <shane.odwyer@wpengine.com>
2021-08-12 16:28:28 +0100

    added new unit test

--- src/components/tests/sum.test.js
+++ src/components/tests/sum.test.js
@@ -3,3 +3,7 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('test entering a string throws an exception', () => {
+    expect(() => sum('one',2)).toThrow('value must be number');
+})
