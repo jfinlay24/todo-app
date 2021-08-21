4293a26c6db60a880fa45f46a540d13948af835c
Jonathan Finlay <jfinlay24@yahoo.ie>
2021-08-21 15:36:10 +0100

    tiding up typescript types

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import TodoListProps from '../TypescriptProps';
 
 interface FormInputProp {
-    addTask: any;
+    addTask: (userInput: string ) => void;
     listOfItems: TodoListProps[];
 }
 
