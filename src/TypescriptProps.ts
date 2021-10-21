8947a0c0a5bcff42f8b4d0cdc78df8f4774f24cf
Jonathan Finlay <jfinlay24@yahoo.ie>
2021-10-21 15:31:09 +0100

    added two new tests

--- src/TypescriptProps.ts
+++ src/TypescriptProps.ts
@@ -1,6 +1,5 @@
 export default interface TodoListProps {
     id: number;
     description: string;
-    checked: boolean;
     textDecor: string;
 }
--- src/components/tests/DisplayList.test.tsx
+++ src/components/tests/DisplayList.test.tsx
@@ -1,8 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import DisplayList from '../DisplayList';
 
-const listOfItems = [ {id:0 , description: 'item1'}, {id:1, description: 'item2'}];
+const listOfItems = [ {id:0 , description: 'item1', textDecor: 'clear'}, {id:1, description: 'item2', textDecor: 'line'}];
 const renderDisplayList = () => { 
     render( <DisplayList listOfItems={listOfItems}/> )
 
@@ -13,11 +16,32 @@ describe('DisplayList', () => {
     it('should render the correct number of items passed to it', () => {
         renderDisplayList();
         const listItems = screen.getAllByRole('listitem');
-
-        console.log(listItems[0])
         
         expect( listItems.length).toBe(2);
     });
 
+
+});describe('DisplayList', () => {
+
+    it('should list the number of items without a strikethrough', () => {
+        renderDisplayList();
+        const listItems = screen.getAllByRole('listitem');
+        let strikethroughValue = 0
+        for(let i = 0; i < listItems.length; i++) {
+            if(listOfItems[i].textDecor === "line"){
+                strikethroughValue =+ 1;
+            }
+        }
+        expect(strikethroughValue).toEqual(1);
+    });
+
+});
+
+describe('DisplayList', () => {
+
+    it('should correctly display text in first todo item', () => {
+        renderDisplayList();
+        expect(listOfItems[0].description).toContain('item1');
+    });
 
 });
