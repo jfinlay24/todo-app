847b22bf0e0a3b1e2427c6906a698e0c7689752e
Jonathan Finlay <jfinlay24@yahoo.ie>
2021-11-18 16:40:33 +0000

    unit tests for error checks

--- src/components/tests/FormInput.test.tsx
+++ src/components/tests/FormInput.test.tsx
@@ -5,6 +5,9 @@ import userEvent from '@testing-library/user-event';
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import FormInput from '../FormInput';
+import { waitFor } from '@testing-library/react';
+import "regenerator-runtime/runtime.js";
+import '@testing-library/jest-dom';
  
 const addTask = (userInput: any) => {
     
@@ -21,16 +24,46 @@ describe('FormInput', () => {
     it('should display character limit error', async () => {
         renderFormInput();
         const expectedUserInput = "This input is too long.";
-        const addButton = screen.getByRole('inputbox')
+        const addButton = screen.getByRole('button');
 
-        userEvent.type(FormInput, expectedUserInput);
         userEvent.type(screen.getByRole('textbox'), expectedUserInput);
         userEvent.click(addButton);
         //screen.debug();
 
-        const checkText = await screen.findByText('Too many characters. Limit is 15.');
-        expect(checkText).toBeInTheDocument();
-        
+        await waitFor(() => {
+            const checkText = screen.queryByText('Too many characters. Limit is 15.');
+            expect(checkText).toBeInTheDocument();
+        })
+    });
+
+    it('should display enter an item error', async () => {
+        renderFormInput();
+        const expectedUserInput = "";
+        const addButton = screen.getByRole('button');
+
+        userEvent.type(screen.getByRole('textbox'), expectedUserInput);
+        userEvent.click(addButton);
+        //screen.debug();
+
+        await waitFor(() => {
+            const checkText = screen.queryByText('Please enter an item.');
+            expect(checkText).toBeInTheDocument();
+        })
+    });
+
+    it('should display a duplicated item error', async () => {
+        renderFormInput();
+        const expectedUserInput = "item1";
+        const addButton = screen.getByRole('button');
+
+        userEvent.type(screen.getByRole('textbox'), expectedUserInput);
+        userEvent.click(addButton);
+        //screen.debug();
+
+        await waitFor(() => {
+            const checkText = screen.queryByText('Item already on list.');
+            expect(checkText).toBeInTheDocument();
+        })
     });
 });
 
